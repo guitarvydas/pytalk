9befb3e2c2e3bc0f049bce19291f7bb3df8cd6cf
Paul Tarvydas <guitarvydas@gmail.com>
2022-09-29 23:56:11 -0400

    cat handler uses message.data, not complete message

--- cat.py
+++ cat.py
@@ -1,8 +1,8 @@
 from leaf import Leaf
 
 class Cat (Leaf):
-    def __handler__ (self, s):
-        self.send (xfrom=self, portname='stdout', data=s, causingMessage=None)
+    def __handler__ (self, message):
+        self.send (xfrom=self, portname='stdout', data=message.data, causingMessage=None)
 
 
         
