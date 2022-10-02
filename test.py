3ed90a3fa3f22e77d4a84966986ea62f9fafc695
Paul Tarvydas <guitarvydas@gmail.com>
2022-10-02 10:10:49 -0400

    split HelloWorld.py into 2 files - HelloWorldSequential.py and HelloWorldConcurrent.py

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
-from helloworld import HelloWorldSequential
-from helloworld import HelloWorldConcurrent
+from helloworldsequential import HelloWorldSequential
+from helloworldconcurrent import HelloWorldConcurrent
 from topmessage import TopMessage
 
 hw = HelloWorldSequential (None, 'hw')
-hw.start (port='stdin', data='sequential hello world')
+hw.start (port='stdin', data='Sequential hello world')
 print (hw.outputs ())
 
 hw = HelloWorldConcurrent (None, 'hw')
-hw.start (port='stdin', data='concurrent hello world')
+hw.start (port='stdin', data='Concurrent hello world')
 print (hw.outputs ())
