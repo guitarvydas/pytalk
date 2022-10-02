33bd53626b74fc94ce0201dcd9bcef484d5389d6
Paul Tarvydas <guitarvydas@gmail.com>
2022-10-02 10:04:50 -0400

    disable debugging

--- connection.py
+++ connection.py
@@ -1,6 +1,6 @@
 class Connection:
     def __init__ (self):
-        self._excrutiatingDetail = True
+        self._excrutiatingDetail = False
         
     def debug (self, note, message, sender, receiver):
         if self._excrutiatingDetail:
